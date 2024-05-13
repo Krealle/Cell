5c0880fe9d4f9111b36a0c5786adc09cfc8bc993
Vollmer <kristianvollmer@gmail.com>
2024-05-13 18:43:49 +0200

    Update logic for focusButton UnitWatching

--- RaidFrames/Units/FocusFrame.lua
+++ RaidFrames/Units/FocusFrame.lua
@@ -7,7 +7,6 @@ local P = Cell.pixelPerfectFuncs
 
 local unit = "focus"
 
--- FocusFrame
 local focusFrame, anchorFrame, hoverFrame, config, menu = B:CreateBaseUnitFrame(unit, "Focus Frame")
 Cell.frames.focusFrame = focusFrame
 Cell.frames.focusFrameAnchor = anchorFrame
@@ -15,13 +14,12 @@ Cell.frames.focusFrameAnchor = anchorFrame
 local focusButton = CreateFrame("Button", "CellFocusButton", focusFrame, "CellUnitButtonTemplate")
 focusButton:SetAttribute("unit", unit)
 focusButton:SetPoint("TOPLEFT")
-focusButton:RegisterEvent("PLAYER_FOCUS_CHANGED")
-focusButton:SetScript("OnEvent", function(self, event)
-    -- Make sure we update target changes
-      B.UpdateAll(self)
-end)
-focusButton:Show()
 Cell.unitButtons.focus[unit] = focusButton
+
+-- Make sure we update frame info when focus target changes
+-- This is needed to prevent getting a delayed frame update
+local targetListener = CreateFrame("FRAME", "CellFocusButtonTargetListener")
+targetListener:SetScript("OnEvent", function() B.UpdateAll(focusButton) end)
 
 -------------------------------------------------
 -- callbacks
@@ -46,11 +44,13 @@ Cell:RegisterCallback("UpdatePixelPerfect", "FocusFrame_UpdatePixelPerfect", Upd
 local function FocusFrame_UpdateVisibility(which)
     if not which or which == unit then
         if Cell.vars.currentLayoutTable[unit]["enabled"] then
-            RegisterAttributeDriver(focusButton, "state-visibility", "[@focus,exists]show;hide")
+            RegisterUnitWatch(focusButton)
+            targetListener:RegisterEvent("PLAYER_FOCUS_CHANGED")
         else
-            UnregisterAttributeDriver(focusButton, "state-visibility")
+            UnRegisterUnitWatch(focusButton)
+            targetListener:UnRegisterEvent("PLAYER_FOCUS_CHANGED")
             focusFrame:Hide()
         end
     end
-  end
-  Cell:RegisterCallback("UpdateVisibility", "FocusFrame_UpdateVisibility", FocusFrame_UpdateVisibility)
+end
+Cell:RegisterCallback("UpdateVisibility", "FocusFrame_UpdateVisibility", FocusFrame_UpdateVisibility)
